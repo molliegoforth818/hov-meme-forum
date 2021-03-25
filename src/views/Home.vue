d1572a8590adaeb6c3a97814b1065b804618cb91
Mollie Goforth <molliegoforth818@gmail.com>
2021-03-24 21:04:16 -0500

    homepage fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,13 @@
 <template>
-  <h1>Home</h1>
+  <h1> what ever could you meme? </h1>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+h1 {
+text-align: center;
+}
+</style>
